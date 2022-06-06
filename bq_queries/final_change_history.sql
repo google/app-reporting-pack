1a28f0a53c4767767a0e4d6eeec99d16af4a4b44
Andrei Markin <amarkin@google.com>
2022-06-06 16:15:19 +0400

    Update final_change_history
    * Add BidBudgetHistory metrics - budget and bids for a given day
    * Add metrics for bid and budget changes (where a change happened on a
      given day)
    * Move performance calculation logic into `CampaignPerformance` subquery
    
    Change-Id: I5232602abc5340be7883716a5396ac918876174f

--- bq_queries/final_change_history.sql
+++ bq_queries/final_change_history.sql
@@ -1,8 +1,31 @@
 -- Contains daily changes (bids, budgets, assets, etc) on campaign level.
 CREATE OR REPLACE TABLE {bq_project}.{bq_dataset}.final_change_history_F
 AS (
+    WITH CampaignPerformance AS (
+        SELECT
+            PARSE_DATE("%Y-%m-%d", date) AS day,
+            campaign_id,
+            SUM(clicks) AS clicks,
+            SUM(impressions) AS impressions,
+            `{bq_project}.{bq_dataset}.NormalizeMillis`(SUM(cost)) AS cost,
+            SUM(view_through_conversions) AS view_through_conversions
+        FROM {bq_project}.{bq_dataset}.ad_group_performance
+        LEFT JOIN {bq_project}.{bq_dataset}.account_campaign_ad_group_mapping AS M
+            USING(ad_group_id)
+        GROUP BY 1, 2
+    ),
+    CampaignMapping AS (
+        SELECT DISTINCT
+            account_id,
+            account_name,
+            currency,
+            campaign_id,
+            campaign_name,
+            campaign_status
+        FROM {bq_project}.{bq_dataset}.account_campaign_ad_group_mapping
+    )
 SELECT
-    PARSE_DATE("%Y-%m-%d", AP.date) AS day,
+    CP.day,
     M.account_id,
     M.account_name,
     M.currency,
@@ -19,16 +42,14 @@ SELECT
     "" AS firebase_bidding_status,
     ACS.start_date AS start_date,
     ARRAY_LENGTH(ACS.target_conversions) AS n_of_target_conversions,
-    ROUND(B.budget_amount / 1e5, 2) AS current_budget_amount,
-    ROUND(B.target_cpa / 1e5, 2) AS current_target_cpa,
-    ROUND(B.target_roas / 1e5, 2) AS current_target_roas,
-    0 AS budget,
-    0 AS target_cpa,
-    0 AS target_roas,
-    0 AS bid_changes,
-    0 AS budget_changes,
-    0 AS image_changes,
+    `{bq_project}.{bq_dataset}.NormalizeMillis`(B.budget_amount) AS current_budget_amount,
+    `{bq_project}.{bq_dataset}.NormalizeMillis`(B.target_cpa) AS current_target_cpa,
+    B.target_roas AS current_target_roas,
+    BidBudgetHistory.budget_amount AS budget,
+    BidBudgetHistory.target_cpa AS target_cpa,
+    BidBudgetHistory.target_roas AS target_roas,
     0 AS text_changes,
+    0 AS image_changes,
     0 AS html5_changes,
     0 AS video_changes,
     0 AS geo_changes,
@@ -36,17 +57,31 @@ SELECT
     0 AS ad_group_resumed,
     0 AS ad_group_paused,
     0 AS ad_group_deleted,
-    SUM(AP.clicks) AS clicks,
-    SUM(AP.impressions) AS impressions,
-    ROUND(SUM(AP.cost) / 1e5, 2) AS cost,
-    SUM(AP.view_through_conversions) AS view_through_conversions
-FROM {bq_project}.{bq_dataset}.ad_group_performance AS AP
-LEFT JOIN {bq_project}.{bq_dataset}.account_campaign_ad_group_mapping AS M
-  ON AP.ad_group_id = M.ad_group_id
+    CASE
+        WHEN BidBudgetHistory.target_cpa = LAG(BidBudgetHistory.target_cpa)
+            OVER (PARTITION BY M.campaign_id ORDER BY CP.day ASC)
+        OR LAG(BidBudgetHistory.target_cpa)
+            OVER (PARTITION BY M.campaign_id ORDER BY CP.day ASC) IS NULL
+        THEN 0
+    ELSE 1
+    END AS bid_changes,
+    CASE
+        WHEN BidBudgetHistory.budget_amount = LAG(BidBudgetHistory.budget_amount)
+            OVER (PARTITION BY M.campaign_id ORDER BY CP.day ASC)
+        OR LAG(BidBudgetHistory.budget_amount)
+            OVER (PARTITION BY M.campaign_id ORDER BY CP.day ASC) IS NULL
+        THEN 0
+    ELSE 1
+    END AS budget_changes,
+FROM CampaignPerformance AS CP
+LEFT JOIN CampaignMapping AS M
+    ON CP.campaign_id = M.campaign_id
 LEFT JOIN {bq_project}.{bq_dataset}.bid_budget AS B
-    ON M.campaign_id = B.campaign_id
+    ON CP.campaign_id = B.campaign_id
+LEFT JOIN `{bq_project}.{bq_dataset}.bid_budgets_*` AS BidBudgetHistory
+    ON M.campaign_id = BidBudgetHistory.campaign_id
+        AND CP.day = BidBudgetHistory.day
 LEFT JOIN `{bq_project}.{bq_dataset}.AppCampaignSettingsView` AS ACS
   ON M.campaign_id = ACS.campaign_id
 LEFT JOIN `{bq_project}.{bq_dataset}.GeoLanguageView` AS G
-  ON M.campaign_id =  G.campaign_id
-GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32);
+  ON M.campaign_id =  G.campaign_id);
