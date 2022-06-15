6527ee874fcf1ce2ff92f6207bcbfe29f54dd18d
Andrei Markin <amarkin@google.com>
2022-06-15 13:31:36 +0400

    Update location_performance query
    * Add aliases
    * Add missing metrics (views, interactions)
    
    Change-Id: I0fbc46833d875252ff2071ad8d752404732a65e8

--- google_ads_queries/performance/location.sql
+++ google_ads_queries/performance/location.sql
@@ -1,10 +1,14 @@
 SELECT
-    segments.date,
-    ad_group.id,
-    user_location_view.country_criterion_id,
-    metrics.impressions,
-    metrics.clicks,
-    metrics.cost_micros
+    segments.date AS date,
+    segments.ad_network_type AS network,
+    campaign.advertising_channel_type AS campaign_type,
+    ad_group.id AS ad_group_id,
+    user_location_view.country_criterion_id AS country_id,
+    metrics.impressions AS impressions,
+    metrics.clicks AS clicks,
+    metrics.cost_micros AS cost,
+    metrics.video_views AS video_views,
+    metrics.interactions AS interactions
 FROM user_location_view
 WHERE
     campaign.advertising_channel_type = "MULTI_CHANNEL"
