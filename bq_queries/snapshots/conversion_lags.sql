cd629919914dadf16a2f58093e33a4776568bf38
Andrei Markin <amarkin@google.com>
2022-06-17 14:35:53 +0400

    Add conversion_lags snapshots
    Change-Id: I5e9b916ca5a79b4305c150fd4980f2e584eabf42

---
+++ bq_queries/snapshots/conversion_lags.sql
@@ -0,0 +1,20 @@
+-- Contains conversions and their values on a given day with automatically calculated lag number
+CREATE OR REPLACE TABLE {bq_project}.{bq_dataset}.conversion_lags_{date_iso}
+AS (
+SELECT
+    PARSE_DATE("%Y-%m-%d", AP.date) AS day_of_interaction,
+    DATE_DIFF(
+        CURRENT_DATE(),
+        PARSE_DATE("%Y-%m-%d", AP.date),
+        DAY) AS lag,
+    M.ad_group_id,
+    AP.asset_id,
+    AP.network AS network,
+    SUM(AP.installs) AS installs,
+    SUM(AP.inapps) AS inapps,
+    SUM(AP.view_through_conversions) AS view_through_conversions,
+    SUM(AP.conversions_value) AS conversions_value
+FROM {bq_project}.{bq_dataset}.asset_performance AS AP
+LEFT JOIN {bq_project}.{bq_dataset}.account_campaign_ad_group_mapping AS M
+  ON AP.ad_group_id = M.ad_group_id
+GROUP BY 1, 2, 3, 4, 5);
