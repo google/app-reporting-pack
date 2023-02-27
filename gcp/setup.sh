7c8cc1d00bdfece33e96b3eb22434e4667156239
Sergei Dorogin <segy@google.com>
2023-02-27 11:57:29 +0400

    ARP-in-GCE: * settings.ini is parameterized via cli * compute: setting instance name and machine type to CF's evn file from settings.ini * enabling a required API eventarc * separate regions for Scheduler and CF * updated gaarf-py dependency (to support ads configs on GCS)
    Change-Id: Ia494f9d34c1f825d27bd3fd10d0320cf6a13c023

--- gcp/setup.sh
+++ gcp/setup.sh
@@ -1,13 +1,25 @@
-TOPIC=run-arp
-REPOSITORY=$(git config -f ./settings.ini repository.name)
-IMAGE_NAME=$(git config -f ./settings.ini repository.image)
-REPOSITORY_LOCATION=$(git config -f ./settings.ini repository.location)
-CF_REGION=$(git config -f ./settings.ini function.region)
-CF_NAME=$(git config -f ./settings.ini function.name)
+SETTING_FILE="./settings.ini"
+while :; do
+    case $1 in
+  -s|--settings)
+      shift
+      SETTING_FILE=$1
+      ;;
+  *)
+      break
+    esac
+  shift
+done
+
+REPOSITORY=$(git config -f $SETTING_FILE repository.name)
+IMAGE_NAME=$(git config -f $SETTING_FILE repository.image)
+REPOSITORY_LOCATION=$(git config -f $SETTING_FILE repository.location)
+TOPIC=$(git config -f $SETTING_FILE pubsub.topic)
 
 PROJECT_ID=$(gcloud config get-value project 2> /dev/null)
 PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="csv(projectNumber)" | tail -n 1)
 SERVICE_ACCOUNT=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
+
 
 enable_apis() {
   gcloud services enable compute.googleapis.com
@@ -17,11 +29,12 @@ enable_apis() {
   gcloud services enable iamcredentials.googleapis.com
   gcloud services enable cloudbuild.googleapis.com
   gcloud services enable cloudfunctions.googleapis.com
+  gcloud services enable eventarc.googleapis.com
 }
 
 
-# create an Artifact Repository
 create_registry() {
+  echo "Creating a repository in Artifact Repository"
   gcloud artifacts repositories create $REPOSITORY \
       --repository-format=Docker \
       --location=$REPOSITORY_LOCATION
@@ -29,18 +42,20 @@ create_registry() {
 
 
 build_docker_image() {
-  # build and push Docker image to Artifact Registry
+  echo "Building and pushing Docker image to Artifact Registry"
   gcloud builds submit --config=cloudbuild.yaml --substitutions=_REPOSITORY="docker",_IMAGE="$IMAGE_NAME",_REPOSITORY_LOCATION="$REPOSITORY_LOCATION" ./.. 
 }
 
 
 build_docker_image_gcr() {
-  # build and push Docker image to Container Registry
+  # NOTE: it's an alternative to build_docker_image if you want to use GCR instead of AR
+  echo "Building and pushing Docker image to Container Registry"
   gcloud builds submit --config=cloudbuild-gcr.yaml --substitutions=_IMAGE="workload" ./workload-vm
 }
 
 
 set_iam_permissions() {
+  echo "Setting up IAM permissions"
   gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$SERVICE_ACCOUNT --role=roles/storage.objectViewer
   gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$SERVICE_ACCOUNT --role=roles/artifactregistry.repoAdmin
   gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$SERVICE_ACCOUNT --role=roles/compute.admin
@@ -49,6 +64,11 @@ set_iam_permissions() {
 
 
 deploy_cf() {
+  echo "Deploying Cloud Function"
+  CF_REGION=$(git config -f $SETTING_FILE function.region)
+  CF_NAME=$(git config -f $SETTING_FILE function.name)
+
+  # it might fail and that's ok (make sure you haven't enabled `set -e` though)
   gcloud pubsub topics create $TOPIC
 
   # create env.yaml from env.yaml.template if it doesn't exist
@@ -59,8 +79,12 @@ deploy_cf() {
   # initialize env.yaml - environment variables for CF
   url="$REPOSITORY_LOCATION-docker.pkg.dev/$PROJECT_ID/docker/$IMAGE_NAME"
   sed -i'.original' -e "s|#*[[:space:]]*DOCKER_IMAGE[[:space:]]*:[[:space:]]*.*$|DOCKER_IMAGE: $url|" ./cloud-functions/create-vm/env.yaml
+  instance=$(git config -f $SETTING_FILE compute.name)
+  sed -i'.original' -e "s|#*[[:space:]]*INSTANCE_NAME[[:space:]]*:[[:space:]]*.*$|INSTANCE_NAME: $instance|" ./cloud-functions/create-vm/env.yaml
+  machine_type=$(git config -f $SETTING_FILE compute.machine-type)
+  sed -i'.original' -e "s|#*[[:space:]]*MACHINE_TYPE[[:space:]]*:[[:space:]]*.*$|MACHINE_TYPE: $machine_type|" ./cloud-functions/create-vm/env.yaml
 
-  # deploy CF
+  # deploy CF (pubsub triggered)
   gcloud functions deploy $CF_NAME \
       --trigger-topic=$TOPIC \
       --entry-point=createInstance \
@@ -76,7 +100,7 @@ deploy_cf() {
 
 deploy_config() {
   echo 'Deploying config to GCS'
-  NAME=$(git config -f ./settings.ini config.name)
+  NAME=$(git config -f $SETTING_FILE config.name)
   GCS_BASE_PATH=gs://$PROJECT_ID/$NAME
   gsutil -h "Content-Type:text/plain" -m cp -R ./../config.yaml $GCS_BASE_PATH/config.yaml
   gsutil -h "Content-Type:text/plain" -m cp -R ./../google-ads.yaml $GCS_BASE_PATH/google-ads.yaml
@@ -88,7 +112,7 @@ get_run_data() {
   #   * project_id
   #   * machine_type
   #   * service_account
-  NAME=$(git config -f ./settings.ini config.name)
+  NAME=$(git config -f $SETTING_FILE config.name)
   GCS_BASE_PATH=gs://$PROJECT_ID/$NAME
   data='{
     "docker_image": "'$REPOSITORY_LOCATION'-docker.pkg.dev/'$PROJECT_ID'/docker/'$IMAGE_NAME'", 
@@ -104,6 +128,7 @@ get_run_data_escaped() {
   echo $ESCAPED_DATA
 }
 
+
 start() {
   # args for the cloud function (create-vm) passed via pub/sub event:
   #   * project_id - 
@@ -118,22 +143,25 @@ start() {
   gcloud pubsub topics publish $TOPIC --message="$DATA"
 }
 
+
 schedule_run() {
-  JOB_NAME=$(git config -f ./settings.ini scheduler.name)
-  SCHEDULE=$(git config -f ./settings.ini scheduler.schedule)
+  JOB_NAME=$(git config -f $SETTING_FILE scheduler.name)
+  REGION=$(git config -f $SETTING_FILE scheduler.region)
+  SCHEDULE=$(git config -f $SETTING_FILE scheduler.schedule)
   SCHEDULE=${SCHEDULE:-"0 0 * * *"} # by default at midnight
   local DATA=$(get_run_data_escaped)
   echo 'Scheduling a job with args: '$DATA
 
-  gcloud scheduler jobs delete $JOB_NAME --location $CF_REGION --quiet
+  gcloud scheduler jobs delete $JOB_NAME --location $REGION --quiet
 
   gcloud scheduler jobs create pubsub $JOB_NAME \
     --schedule="$SCHEDULE" \
-    --location=$CF_REGION \
+    --location=$REGION \
     --topic=$TOPIC \
     --message-body="{\"argument\": \"$DATA\"}" \
     --time-zone="Etc/UTC"
 }
+
 
 deploy_all() {
   enable_apis
