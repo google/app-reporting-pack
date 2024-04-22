520423134ac4182e1d3845d4ba26aa2e37194bb8
Andrei Markin <amarkin@google.com>
2024-04-22 18:51:56 +0400

    Add tests for google_ads_queries for different versions of APIs
    Change-Id: I5902ea65dc35c89f84b61b5357017b36446a8fcc

---
+++ tests/test_google_queries.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SCRIPT_PATH=$(readlink -f "$0" | xargs dirname)
+
+api_version=$1
+if (($api_version  == 14 )); then
+  skan4=false
+else
+  skan4=true
+fi
+echo "api_version: $api_version"
+echo "skan4: $skan4"
+errors=$(gaarf-simulator $SCRIPT_PATH/../app/*/google_ads_queries/*.sql \
+  --account 1 \
+  --macro.start_date=:YYYYMMDD-1 \
+  --macro.end_date=:YYYYMMDD-1 \
+  --api-version=$api_version \
+  --template.skan4=$skan4 > /dev/null)
+if [[ ! -z $errors ]]; then
+  echo "Found errors in the Google Ads API queries."
+  exit 1
+fi
