5ee91a5a137c1cfa54887bda5290dcbf8079d9c8
Andrei Markin <amarkin@google.com>
2024-07-01 21:12:44 +0400

    Format conversion lag adjustment module
    * Ensure that config is build only from gaarf_config argument
    * Switch to using BigQueryWriter instead of relying on
      `write_data_to_bq` helper method
    * Simplify code in `conv_lag_adjustment`
    * Return `GaarfReport` in src/conv_lag_builder.py` to simplify writing
    * Format imports
    * Add missing docstrings
    
    Change-Id: I06b7712466c19a3bc27ad93754bdcf0c7010b903

--- app/scripts/conv_lag_adjustment.py
+++ app/scripts/conv_lag_adjustment.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,84 +11,71 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Calculates values of conversion lags for a given day of lag.
+
+Script creates table in BigQuery that contains adjustment coefficients for
+each lag (from 1 till 90 day) for a given conversion_id and network
+"""
 
 import argparse
 from datetime import datetime, timedelta
 
-import pandas as pd
-import yaml
-from gaarf.api_clients import GoogleAdsApiClient
+import gaarf
+from gaarf import api_clients
 from gaarf.cli import utils as gaarf_utils
-from gaarf.query_executor import AdsReportFetcher
-from google.cloud import bigquery
-from smart_open import open
-from src.conv_lag_builder import ConversionLagBuilder
-from src.queries import ConversionLagQuery
-from src.utils import write_data_to_bq
+from gaarf.io.writers import bigquery_writer
+from src import conv_lag_builder, queries
 
 
-def generate_placeholders():
-  df = pd.DataFrame(
-    data=[['', '', 0, 0.0]],
-    columns=['network', 'conversion_id', 'lag_day', 'lag_adjustment'],
+def generate_placeholders() -> gaarf.report.GaarfReport:
+  """Returns empty report if no lag data were found."""
+  return gaarf.report.GaarfReport(
+    results=[['', '', 0, 0.0]],
+    column_names=['network', 'conversion_id', 'lag_day', 'lag_adjustment'],
   )
-  return df
 
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-c', '--config', dest='gaarf_config', default=None)
-  parser.add_argument('--output', dest='save', default='bq')
   parser.add_argument('--ads-config', dest='ads_config')
-  parser.add_argument('--account', dest='customer_id')
-  parser.add_argument('--api-version', dest='api_version', default='12')
-  parser.add_argument(
-    '--customer-ids-query', dest='customer_ids_query', default=None
-  )
-  parser.add_argument(
-    '--customer-ids-query-file', dest='customer_ids_query_file', default=None
-  )
   parser.add_argument('--log', '--loglevel', dest='loglevel', default='info')
   parser.add_argument('--logger', dest='logger', default='local')
 
-  args = parser.parse_known_args()
+  args, kwargs = parser.parse_known_args()
 
-  config = gaarf_utils.ConfigBuilder('gaarf').build(vars(args[0]), args[1])
+  config = gaarf_utils.ConfigBuilder('gaarf').build(vars(args), kwargs)
 
   project, dataset = (
     config.writer_params.get('project'),
     config.writer_params.get('dataset'),
   )
 
-  with open(args[0].ads_config, 'r', encoding='utf-8') as f:
-    google_ads_config_dict = yaml.safe_load(f)
-  google_ads_client = GoogleAdsApiClient(
-    config_dict=google_ads_config_dict, version=f'v{config.api_version}'
+  report_fetcher = gaarf.report_fetcher.AdsReportFetcher(
+    api_clients.GoogleAdsApiClient(
+      path_to_config=args.ads_config, version=config.api_version
+    )
   )
-  report_fetcher = AdsReportFetcher(google_ads_client)
   customer_ids = report_fetcher.expand_mcc(
-    config.account, args[0].customer_ids_query
+    config.account, config.customer_ids_query
   )
-
-  bq_client = bigquery.Client(project)
 
   days_ago_180 = (datetime.now() - timedelta(days=180)).strftime('%Y-%m-%d')
   days_ago_30 = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
 
   lag_data = report_fetcher.fetch(
-    ConversionLagQuery(days_ago_180, days_ago_30), customer_ids
+    queries.ConversionLagQuery(days_ago_180, days_ago_30), customer_ids
   )
   if lag_data:
-    conv_lag_builder = ConversionLagBuilder(
+    conv_lag_table = conv_lag_builder.ConversionLagBuilder(
       lag_data.to_pandas(), ['network', 'conversion_id']
-    )
-
-    conv_lag_table = conv_lag_builder.calculate_reference_values()
+    ).calculate_reference_values()
   else:
     conv_lag_table = generate_placeholders()
-  write_data_to_bq(
-    bq_client, conv_lag_table, f'{project}.{dataset}.conversion_lag_adjustments'
-  )
+  bigquery_writer.BigQueryWriter(
+    project=project,
+    dataset=dataset,
+  ).write(conv_lag_table, 'conversion_lag_adjustments')
 
 
 if __name__ == '__main__':
